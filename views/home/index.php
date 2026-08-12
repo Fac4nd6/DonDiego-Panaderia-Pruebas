af853270e45e4c798916cacf225c5b249497e20d
cano <thiagocano2325@gmail.com>
2026-08-11 23:49:14 -0300

    conocenos creado y fotos nuevas

--- views/home/index.php
+++ views/home/index.php
@@ -90,16 +90,13 @@ require '../layouts/header.php';
                 </article>
 
             </div>
-
-            <a href="catalogo.php" class="catalogo-button">
-                Ver catálogo completo
-            </a>
+            
         </section>
-
-        <section class="sobre-nosotros">
-            <h2>Sobre nosotros</h2>
-            <!-- Información -->
-        </section>
+                <a href="catalogo.php" class="catalogo-button">
+                    Ver catálogo completo
+                </a>
+        
+       
 
         <!-- Productos recomendados para el usuario -->
 
@@ -130,11 +127,24 @@ require '../layouts/header.php';
             </div>
         </section>
 
-    </main>
+         <section class="conocenos">
+            
+            <div class="informacion">
+                <div class="info-texto">
+                    <h2>Animate y visitanos</h2>
+                    <p>Uruguay 1796<p>
+                    <p>de 6:00 a 20:00 hs</p>
+                    <P>+598 95 005 706</p>
+                    <P>473 49 924</p>
+                 </div>
+                <iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6811.739034260684!2d-57.9604178!3d-31.3901612!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95addd560a248351%3A0x31070367860ed798!2sPanader%C3%ADa%20y%20Confiter%C3%ADa%20Don%20Diego%20(Centro)!5e0!3m2!1ses-419!2suy!4v1786501316195!5m2!1ses-419!2suy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                <img class="local" src="../../public/img/dondiego-local.jpg" alt="Productos de Don Diego">    
+                </div>
+            
+        </section>
 
-    <footer>
-        <!-- Footer -->
-    </footer>
+    </main>
+<?php require '../layouts/footer.php'; ?>
 
 </body>
 
